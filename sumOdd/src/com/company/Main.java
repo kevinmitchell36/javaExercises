9bbec8ddefdfd425ba02795f4c99b09d564a61d4
Kevin Mitchell <kevin@Kevins-MBP.attlocal.net>
2020-04-09 20:16:39 -0500

    sum odd

---
+++ sumOdd/src/com/company/Main.java
@@ -0,0 +1,36 @@
+package com.company;
+
+public class Main {
+
+    public static void main(String[] args) {
+        System.out.println(sumOdd(1, 100));
+        System.out.println(sumOdd(-1, 100));
+        System.out.println(sumOdd(100, 100));
+        System.out.println(sumOdd(13, 13));
+        System.out.println(sumOdd(100, -100));
+        System.out.println(sumOdd(100, 1000));
+    }
+
+    public static boolean isOdd(int number) {
+        if (number <= 0) {
+            return false;
+        }
+        if(number % 2 != 0) {
+            return true;
+        }
+        return false;
+    }
+
+    public static int sumOdd(int start, int end) {
+        if(end < start || start < 0 || end < 0) {
+            return -1;
+        }
+        int sum = 0;
+        for(int i = start; i <= end; i++) {
+            if(isOdd(i)) {
+                sum += i;
+            }
+        }
+        return sum;
+    }
+}
